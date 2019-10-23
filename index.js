2bfbc2e2e978460bc427fcf428478d4a8bcdd986
Raman Katsin <roman.katin@ewave.com>
2019-10-23 17:28:54 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,84 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+// index.js is used to setup and configure your bot
+
+// Import required packages
+const path = require('path');
+const restify = require('restify');
+
+// Import required bot services.
+// See https://aka.ms/bot-services to learn more about the different parts of a bot.
+const { BotFrameworkAdapter } = require('botbuilder');
+const { ActivityTypes } = require('botbuilder-core');
+
+// This bot's main dialog.
+const { ProactiveBot } = require('./bots/proactiveBot');
+
+// Note: Ensure you have a .env file and include the MicrosoftAppId and MicrosoftAppPassword.
+const ENV_FILE = path.join(__dirname, '.env');
+require('dotenv').config({ path: ENV_FILE });
+
+// Create adapter.
+// See https://aka.ms/about-bot-adapter to learn more about adapters.
+const adapter = new BotFrameworkAdapter({
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword
+});
+
+// Catch-all for errors.
+adapter.onTurnError = async (context, error) => {
+    // Create a trace activity that contains the error object
+    const traceActivity = {
+        type: ActivityTypes.Trace,
+        timestamp: new Date(),
+        name: 'onTurnError Trace',
+        label: 'TurnError',
+        value: `${ error }`,
+        valueType: 'https://www.botframework.com/schemas/error'
+    };
+    // This check writes out errors to console log .vs. app insights.
+    // NOTE: In production environment, you should consider logging this to Azure
+    //       application insights.
+    console.error(`\n [onTurnError] unhandled error: ${ error }`);
+
+    // Send a trace activity, which will be displayed in Bot Framework Emulator
+    await context.sendActivity(traceActivity);
+
+    // Send a message to the user
+    await context.sendActivity(`The bot encounted an error or bug.`);
+    await context.sendActivity(`To continue to run this bot, please fix the bot source code.`);
+};
+
+// Create the main dialog.
+const conversationReferences = {};
+const bot = new ProactiveBot(conversationReferences);
+
+// Create HTTP server.
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function() {
+    console.log(`\n${ server.name } listening to ${ server.url }`);
+    console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
+});
+
+// Listen for incoming activities and route them to your bot main dialog.
+server.post('/api/messages', (req, res) => {
+    adapter.processActivity(req, res, async (turnContext) => {
+        // route to main dialog.
+        await bot.run(turnContext);
+    });
+});
+
+// Listen for incoming notifications and send proactive messages to users.
+server.get('/api/notify', async (req, res) => {
+    for (const conversationReference of Object.values(conversationReferences)) {
+        await adapter.continueConversation(conversationReference, async turnContext => {
+            await turnContext.sendActivity('proactive hello');
+        });
+    }
+
+    res.setHeader('Content-Type', 'text/html');
+    res.writeHead(200);
+    res.write('<html><body><h1>Proactive messages have been sent.</h1></body></html>');
+    res.end();
+});
